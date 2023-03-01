9cd69375cf990d60f0158d2bfd79aed2ed1384ba
roshankumar811 <roshanshoti100@gmail.com>
2023-02-28 21:35:10 -0500

    initial commit

---
+++ src/com/luv2code/testdb/TestDbServlet.java
@@ -0,0 +1,45 @@
+package com.luv2code.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*; 
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//setup connection variables
+		String user = "springstudents"; 
+		String pass = "springstudents"; 
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false"; 
+		String driver = "com.mysql.jdbc.Driver"; 
+		
+		try {
+			PrintWriter out = response.getWriter(); 
+			out.println("Connecting to database: " + jdbcUrl); 
+			Class.forName(driver); 
+			Connection myCon = DriverManager.getConnection(jdbcUrl, user,pass); 
+			out.println("Connection SuccessFull!!!"); 
+		}
+		catch(Exception e) {
+			e.printStackTrace(); 
+			throw new ServletException(e); 
+		}
+		
+	}
+
+}
